850052d4e0a7c08d502d3f2fb055a0010fa5405a
Shruti Rathod <137332696+shru12ti@users.noreply.github.com>
2023-11-05 00:11:32 +0530

    Add files via upload

---
+++ c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 0;
+    int result;
+
+    result = x / y;  // Division by zero error
+
+    printf("The result is: %d\n", result);
+
+    return 0;
+}
